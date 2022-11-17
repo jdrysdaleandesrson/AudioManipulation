cab87c367576b85f7c276441b07e8de9d3d01754
jdrysdaleandesrson <jonadrys@gmail.com>
2022-11-16 19:31:33 -0600

    copyPaste

--- project/src/main.c
+++ project/src/main.c
@@ -5,6 +5,8 @@
 #include "driver_examples.h"
 
 float mBuffer[1024];
+kiss_fft_cpx cpx_in[1024]; 
+kiss_fft_cpx cpx_out[1024];
 
 int main(void){
 	//Kiss mFft;
@@ -14,9 +16,6 @@ int main(void){
 	atmel_start_init();
 	int adc_val;
 	adc_sync_enable_channel(&ADC_0, CONF_ADC_0_CHANNEL_0);
-	//kiss_fft_cfg cfg = kiss_fft_alloc(1024,0 ,0,0 );
-	kiss_fft_cpx *cpx_in = malloc(FRAMES * sizeof(kiss_fft_cpx));
-	kiss_fft_cpx *cpx_out = malloc(FRAMES * sizeof(kiss_fft_cpx));
 	kiss_fft_cfg cfg = kiss_fft_alloc(1024, 0 ,0,0 );
 	/* Replace with your application code */
 	while(1){
@@ -24,7 +23,7 @@ int main(void){
 	    	//	adc_sync_read_channel(&ADC_0, 0,&adc_val,4);
 			adc_sync_read_channel(&ADC_0, CONF_ADC_0_CHANNEL_0, &adc_val, 2);
 	    		mBuffer[i] = adc_val;
-			cpx_in[i] = (kiss_fft_cpx){.r = x, .i = x}; 
+			cpx_in[i] = (kiss_fft_cpx){.r = mBuffer[i], .i = mBuffer[i]}; 
 			//io_write("%d\n",adc_val);
 		}
 //		kiss_fft(cfg,cx_in,cx_out);
