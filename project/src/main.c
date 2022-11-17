b8665465355e8e68c757dc2fc44e9df773426ba0
jdrysdaleandesrson <jonadrys@gmail.com>
2022-11-16 19:06:19 -0600

    lab

--- project/src/main.c
+++ project/src/main.c
@@ -1,26 +1,35 @@
 #include <atmel_start.h>
-#include "driver_examples.h"
-#include "driver_init.h"
+#include "kiss_fft.h"
 #include "utils.h"
+#include "driver_init.h"
+#include "driver_examples.h"
 
+float mBuffer[1024];
 
-int main(void)
-{
-	float buffer[4096];
+int main(void){
+	//Kiss mFft;
+	//mFft.setData(mBuffer);
+	//float * mData = mFft.getAmplitude();	
 	/* Initializes MCU, drivers and middleware */
 	atmel_start_init();
 	int adc_val;
-	int i =0;
-        adc_sync_enable_channel(&ADC_0, CONF_ADC_0_CHANNEL_0);
-  
-        while (1) {
-            adc_sync_read_channel(&ADC_0, CONF_ADC_0_CHANNEL_0,&adc_val, 2);
-	    buffer[i] =(float)adc_val;
-	    i++;
-	}
-	adc_sync_read_channel(&ADC_0, 0,&adc_val,4);
+	adc_sync_enable_channel(&ADC_0, CONF_ADC_0_CHANNEL_0);
+	//kiss_fft_cfg cfg = kiss_fft_alloc(1024,0 ,0,0 );
+	kiss_fft_cpx *cpx_in = malloc(FRAMES * sizeof(kiss_fft_cpx));
+	kiss_fft_cpx *cpx_out = malloc(FRAMES * sizeof(kiss_fft_cpx));
+	kiss_fft_cfg cfg = kiss_fft_alloc(1024, 0 ,0,0 );
 	/* Replace with your application code */
-	while (1) {
+	while(1){
+		for(int i =0; i<=1024; i++){	
+	    	//	adc_sync_read_channel(&ADC_0, 0,&adc_val,4);
+			adc_sync_read_channel(&ADC_0, CONF_ADC_0_CHANNEL_0, &adc_val, 2);
+	    		mBuffer[i] = adc_val; 
+			//io_write("%d\n",adc_val);
+		}
+//		kiss_fft(cfg,cx_in,cx_out);
 	}
-	//4096 global type_un16t
+	//fft
+	//inverse fft
+	//dac
+	//printf(cx_out.real);
 }
